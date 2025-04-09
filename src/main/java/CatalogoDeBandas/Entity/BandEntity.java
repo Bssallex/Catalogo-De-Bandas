8a4bb1f157d0fd60dbf06bb9531a5a55136d4eba
Alex Bruno <Bssallex@gmail.com>
2025-04-09 14:44:48 -0300

    feat: Adiciona a camada de entidade do banco de dados

---
+++ src/main/java/CatalogoDeBandas/Entity/BandEntity.java
@@ -0,0 +1,30 @@
+package CatalogoDeBandas.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Catalogo")
+public class BandEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_banda")
+    private String nome;
+
+    @Column(name = "ano_criação")
+    private int ano;
+
+    @Column(name = "descrição")
+    private String descricao;
+
+    @Column(name = "membros")
+    private int membros;
+}
