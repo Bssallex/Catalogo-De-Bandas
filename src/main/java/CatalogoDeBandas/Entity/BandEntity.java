1dcfdac980fe9a5aa7b8ba53d9d1d55c9b1ade0c
Alex Bruno <Bssallex@gmail.com>
2025-04-09 15:11:35 -0300

    fix: Adiciona a @Build na entidade para utilizar na classe Mapper

--- src/main/java/CatalogoDeBandas/Entity/BandEntity.java
+++ src/main/java/CatalogoDeBandas/Entity/BandEntity.java
@@ -2,11 +2,13 @@ package CatalogoDeBandas.Entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "Catalogo")
