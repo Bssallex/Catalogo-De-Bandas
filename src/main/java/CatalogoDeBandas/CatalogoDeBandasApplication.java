dc562660d944000b4ba88aec7a0ddd1b9fb33403
Alex Bruno <Bssallex@gmail.com>
2025-04-09 14:30:50 -0300

    first commit

---
+++ src/main/java/CatalogoDeBandas/CatalogoDeBandasApplication.java
@@ -0,0 +1,13 @@
+package CatalogoDeBandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeBandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoDeBandasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CatalogoDeBandas/CatalogoDeBandasApplicationTests.java
@@ -0,0 +1,13 @@
+package CatalogoDeBandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeBandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
