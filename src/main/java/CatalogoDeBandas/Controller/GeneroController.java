92eed3bcfbcc6c15d5a57b92569fde3b04507472
Alex Bruno <Bssallex@gmail.com>
2025-04-18 07:16:53 -0300

    feat: Documenta com o swagger a tabela de gêneros

--- src/main/java/CatalogoDeBandas/Controller/GeneroController.java
+++ src/main/java/CatalogoDeBandas/Controller/GeneroController.java
@@ -1,16 +1,18 @@
 package CatalogoDeBandas.Controller;
 
-import CatalogoDeBandas.Mapper.GeneroMapper;
 import CatalogoDeBandas.Request.GeneroRequest;
 import CatalogoDeBandas.Response.GeneroResponse;
 import CatalogoDeBandas.Service.GeneroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/genero")
@@ -21,26 +23,42 @@ public class GeneroController {
     private String mensagem = "Não foi encontrado nenhum gênero com esse id: ";
 
     @GetMapping("/listar")
+    @Operation(summary = "Lista todos os gêneros", description = "Lista todos os gêneros que estão cadastrados no banco de dados")
+    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "Requisição bem-sucedida")})
     public ResponseEntity<List<GeneroResponse>> listar(){
         List<GeneroResponse> listar = service.listar();
         return ResponseEntity.ok(listar);
     }
 
     @GetMapping("/listar/{id}")
-    public ResponseEntity<GeneroResponse> listarId(@PathVariable Long id){
+    @Operation(summary = "Lista um gênero por ID", description = "Lista um gênero a partir do ID enviado pelo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Requisição bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "O recurso não foi encontrado")
+    })
+    public ResponseEntity<GeneroResponse> listarId(@Parameter(description = "Usuário envia um ID")@PathVariable Long id){
        return service.listarPorId(id)
                .map(ResponseEntity::ok)
                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
-    @PostMapping("/salvar")
-    public ResponseEntity<GeneroResponse> salvar(@RequestBody GeneroRequest request){
+    @PostMapping("/criar")
+    @Operation(summary = "Cria um novo gênero", description = "Cria um novo gênero com os dados enviados pelo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso")
+    })
+    public ResponseEntity<GeneroResponse> criar(@RequestBody GeneroRequest request){
         service.salvar(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/alterar/{id}")
-    public ResponseEntity<?> alterarPorId(@PathVariable Long id, @RequestBody GeneroRequest request){
+    @Operation(summary = "Altera um gênero por ID", description = "Procura um gênero por ID e em seguida é alterado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Requisição bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "O recurso não foi encontrado")
+    })
+    public ResponseEntity<?> alterarPorId(@Parameter(description = "Usuário envia um ID")@PathVariable Long id, @RequestBody GeneroRequest request){
         return service.listarPorId(id).map(alterar -> {
             service.alterar(id, request);
             return ResponseEntity.ok().build();
@@ -48,7 +66,12 @@ public class GeneroController {
     }
 
     @DeleteMapping("/deletar/{id}")
-    public ResponseEntity<?> deletar(@PathVariable Long id){
+    @Operation(summary = "Deleta um gênero por ID", description = "Procura um gênero por ID e em seguida é deletado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Requisição bem-sucedida"),
+            @ApiResponse(responseCode = "404", description = "O recurso não foi encontrado")
+    })
+    public ResponseEntity<?> deletar(@Parameter(description = "Usuário envia um ID")@PathVariable Long id){
         return service.listarPorId(id).map(deletar -> {
             service.deletar(id);
             return ResponseEntity.ok().build();
